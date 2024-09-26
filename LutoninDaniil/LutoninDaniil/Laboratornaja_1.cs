c5345fb1b3cca67335c9e4ee6432aeb0ac80d66c
Lutonin Daniil <lutonindm@gmail.com>
2024-09-26 22:49:06 +0600

    first commit

---
+++ LutoninDaniil/LutoninDaniil/Laboratornaja_1.cs
@@ -0,0 +1,47 @@
+<<<<<<< HEAD
+﻿using System;
+
+=======
+using System;
+>>>>>>> 041fbe123711029ee3bea90f09981c134503fb9c
+class Z1{
+  static void Main() {
+      int a,b;
+      a=Convert.ToInt32(Console.ReadLine());
+      b=Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(a+b-a);Console.WriteLine(b+a-b);
+  }
+}
+<<<<<<< HEAD
+ /*
+=======
+
+using System;
+>>>>>>> 041fbe123711029ee3bea90f09981c134503fb9c
+class Z2{
+  static void Main() {
+      int x,y;
+      x=Convert.ToInt32(Console.ReadLine());
+      y=Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(((Math.Abs(x-y))/2.0)+(Math.Abs(y+x)/2.0));
+  }
+}
+
+<<<<<<< HEAD
+=======
+using System;
+>>>>>>> 041fbe123711029ee3bea90f09981c134503fb9c
+class Z3{
+  static void Main() {
+      int p,l,m,n;
+      n=Convert.ToInt32(Console.ReadLine());
+      l=3;
+      m=3;
+      p=5;
+    Console.WriteLine((2*n*p)+n*(2*l+2*m)+n*(l*n-l));
+  }
+}
+<<<<<<< HEAD
+*/
+=======
+>>>>>>> 041fbe123711029ee3bea90f09981c134503fb9c
