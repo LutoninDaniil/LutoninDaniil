2797733557441ab44047e1534c5e033365df61ef
Lutonin Daniil <lutonindm@gmail.com>
2025-04-07 22:13:23 +0600

    Небольшие доработки + коммивояжер

--- LutoninDaniil/LutoninDaniil/1_kurs/2_semestr/Diskr/Laboratornaja_2.cs
+++ LutoninDaniil/LutoninDaniil/1_kurs/2_semestr/Diskr/Laboratornaja_2.cs
@@ -1,8 +1,10 @@
 /*13.02.2025*/
-/*
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
+/*
+// Алгоритм Прима
 public class Tochka
 {
     public List<int> DrugieTochki { get; } = new List<int>();
@@ -15,50 +17,57 @@ public static class Algoritm
         int SSSSS = 0;
         int[][] Lol = str.Select(line => line.Split(' ').Select(int.Parse).ToArray()).ToArray();
         int i = 0;
-        while (REPO.Count < tochki.Length)
+        int visitedCheck = 0;
+        while (REPO.Count < tochki.Length && visitedCheck < tochki.Length * 2)
         {
             if (!REPO.Contains(i))
             {
                 REPO.Add(i);
             }
-            int miiiiin = 0;
+            int miiiiin = int.MaxValue;
             foreach (int volt in Lol[i])
             {
-                if (volt > 0 && (miiiiin == 0 || volt < miiiiin))
+                if (volt > 0 && volt < miiiiin)
                 {
                     miiiiin = volt;
                 }
             }
-            SSSSS += miiiiin;
-            for (int j = tochki[i].DrugieTochki.Count - 1; j >= 0; j--)
+            if (miiiiin != int.MaxValue)
+            {
+                SSSSS += miiiiin;
+            }
+            bool found = false;
+            for (int j = 0; j < tochki[i].DrugieTochki.Count; j++)
             {
                 int a = tochki[i].DrugieTochki[j];
                 if (!REPO.Contains(a) && Lol[i][a] == miiiiin && miiiiin > 0)
                 {
                     REPO.Add(a);
                     Lol[i][a] = 0;
-                    Lol[a][i] = 0;
                     i = a;
+                    found = true;
                     break;
                 }
             }
-            if (miiiiin == 0)
+            if (!found)
             {
+                i = (REPO.Count < tochki.Length) ? 0 : i; 
                 i++;
+                if (i >= tochki.Length)
+                {
+                    i = 0;
+                }
             }
-            if (i >= tochki.Length)
-            {
-                i = 0;
-            }
+            visitedCheck++;
         }
-        Console.WriteLine("\tОтветочка: " + SSSSS);
+        Console.WriteLine("\tОтветочка: " + (SSSSS > 0 ? SSSSS : 0));
     }
 }
 class Prop
 {
     static void Main()
     {
-        Console.WriteLine("Введите матрицу(числа через пробел):");
+        Console.WriteLine("Введите матрицу(числа через пробельчик):");
         List<string> tremor = new List<string>();
         while (true)
         {
@@ -90,6 +99,101 @@ class Prop
         }
         Console.WriteLine("\nДлина пути:");
         Algoritm.YUI(points, tremor.ToArray());
+    }
+}
+*/
+// Алгоритм Краскала
+/*
+public class Repa
+{
+    public int[] Papa;
+    public Repa(int shirina)
+    {
+        Papa = Enumerable.Range(0, shirina).ToArray();
+    }
+    public int Poisk(int x)
+    {
+        if (Papa[x] != x)
+        {
+            Papa[x] = Poisk(Papa[x]);
+        }
+        return Papa[x];
+    }
+    public void Utub(int x, int y)
+    {
+        int Sos = Poisk(x);
+        int Oso = Poisk(y);
+        if (Sos != Oso)
+        {
+            Papa[Oso] = Sos;
+        }
+    }
+}
+public static class Vter
+{
+    public static void SSrt(List<Tuple<int, int, int>> rer)
+    {
+        for (int i = 0; i < rer.Count; i++)
+        {
+            for (int j = 0; j < rer.Count - 1; j++)
+            {
+                if (rer[j].Item3 > rer[j + 1].Item3)
+                {
+                    (rer[j], rer[j + 1]) = (rer[j + 1], rer[j]);
+                }
+            }
+        }
+    }
+    public static void Mramor(string[] str)
+    {
+        List<Tuple<int, int, int>> VSrer = new List<Tuple<int, int, int>>();
+        for (int i = 0; i < str.Length; i++)
+        {
+            int[] hlop = str[i].Split(' ').Select(int.Parse).ToArray();
+            for (int j = i + 1; j < hlop.Length; j++)
+            {
+                if (hlop[j] > 0)
+                {
+                    VSrer.Add(Tuple.Create(i, j, hlop[j]));
+                }
+            }
+        }
+        SSrt(VSrer);
+        Repa dsu = new Repa(str.Length);
+        int Suuumka = 0;
+        foreach (var rebro in VSrer)
+        {
+            if (dsu.Poisk(rebro.Item1) != dsu.Poisk(rebro.Item2))
+            {
+                Suuumka += rebro.Item3;
+                dsu.Utub(rebro.Item1, rebro.Item2);
+            }
+        }
+        Console.WriteLine("Суммарик: " + Suuumka);
+    }
+}
+class Frog
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите матрицу(0 и 1 через пробел/пустая строка - конец матрицы):");
+        List<string> motik = new List<string>();
+        while (true)
+        {
+            Console.Write("> ");
+            string tuput = (Console.ReadLine() ?? "").Trim();
+            if (string.IsNullOrEmpty(tuput))
+            {
+                break;
+            }
+            if (tuput.Split(' ').Any(x => !int.TryParse(x, out _)))
+            {
+                Console.WriteLine("Только цифры и пробелы!🤬🪓");
+                continue;
+            }
+            motik.Add(tuput);
+        }
+        Vter.Mramor(motik.ToArray());
     }
 }
 */
--- LutoninDaniil/LutoninDaniil/obj/Debug/net8.0/LutoninDaniil.AssemblyInfo.cs
+++ LutoninDaniil/LutoninDaniil/obj/Debug/net8.0/LutoninDaniil.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LutoninDaniil")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7db2c285e9600d3d7b036d30c8ecade36b54d1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c4cc3843beb65e08d6c10602acbfa628f59b939")]
 [assembly: System.Reflection.AssemblyProductAttribute("LutoninDaniil")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LutoninDaniil")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
